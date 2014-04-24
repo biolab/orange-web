4e1ba430d639c9959d48d07d7744b4b10f1b9897
Miha Jenko <mjenko@t-2.net>
2014-04-24 20:54:01 +0200

    Added jquery-visage for screenshots, contributing.html form ported to Bootstrap, prettified feed parsing on homepage.html.

--- homepage/views.py
+++ homepage/views.py
@@ -18,9 +18,11 @@ for node in doc.getElementsByTagName('screenshot'):
         'id': iid,
         'title': node.getAttribute('title'),
         'hide': node.getAttribute('hide'),
-        'img': node.getAttribute('img') or ('homepage/screenshots/snp-%s.png' % iid),
+        'img': node.getAttribute('img')
+        or ('homepage/screenshots/snp-%s.png' % iid),
         'rank': int(node.getAttribute('rank') or 999),
-        'thumb': node.getAttribute('thumb') or ('homepage/screenshots/tbn-%s.png' % iid),
+        'thumb': node.getAttribute('thumb')
+        or ('homepage/screenshots/tbn-%s.png' % iid),
         'features': node.getAttribute('features'),
     }
     if not screenshot['hide'] == 'yes':
@@ -28,6 +30,7 @@ for node in doc.getElementsByTagName('screenshot'):
 
 
 def screenshots(request):
+    """Sort screenshots by their rank, put them into a list of clusters of 4"""
     _screenshots.sort(key=lambda x: x['rank'])
     return render(request, 'screenshots.html', {'screenshots': _screenshots})
 
@@ -60,33 +63,49 @@ def license(request):
 
 
 def contribute(request):
-    if request.method == 'POST' and request.POST['signature'] == "I AGREE":
+    if request.method == 'POST' and request.POST['Signature'] == "I AGREE":
         message = "This message was sent to you automatically from orange.biolab.si.\n\n" + \
-                  request.POST['fullname'] + " has agreed to and electronically signed Orange Contributor " \
+                  request.POST['Full Name'] + " electronically signed Orange Contributor " \
                   "License Agreement. Below are his/her contact information:" \
-                  "\n\nFull Name: " + request.POST['fullname'] + \
-                  "\nE-mail: " + request.POST['email'] + \
-                  "\nMailing Address: \n\n" + request.POST['address'] + \
-                  "\n\nCountry: " + request.POST['country'] + \
-                  "\nTelephone Number: " + request.POST['number'] + \
-                  "\n\nThe user has agreed to electronically sign the agreement by typing I AGREE in the appropriate " \
-                  "Electronic Signature form field.\n\nGood day,\nBiolab Webmaster"
-        send_mail('Orange Contributor License Agreement Receipt', message, 'from@example.com',
-                  ['mjenko@t-2.net'], fail_silently=False)
+                  "\n\nFull Name: " + request.POST['Full Name'] + \
+                  "\nE-mail: " + request.POST['E-mail'] + \
+                  "\nMailing Address: \n\n" + request.POST['Address'] + \
+                  "\n\nCountry: " + request.POST['Country'] + \
+                  "\nTelephone Number: " + request.POST['Number'] + \
+                  "\n\nThe user has confirmed this action by typing " \
+                  "\"I AGREE\" in the appropriate Electronic Signature form field." \
+                  "\n\nGood day,\nBiolab Webmaster"
+        send_mail('Orange Contributor License Agreement Receipt', message,
+                  'from@example.com', ['mjenko@t-2.net'], fail_silently=False)
     return render(request, 'contributing-to-orange.html')
 
 
-entries = []
 feed = feedparser.parse('http://orange.biolab.si/blog/rss/')
-for i in range(5):
-    # Parses first 5 entries from remote blog feed.
+# Parses first 6 entries from remote blog feed.
+entries_1 = []
+entries_2 = []
+for i in range(6):
     entry = {
         'title': feed['entries'][i]['title'],
-        'link': feed['entries'][i]['link']
+        'link': feed['entries'][i]['link'],
+        'text': feed['entries'][i]['summary_detail']['value']
     }
-    entries.append(entry)
+    if i % 2 == 0:
+        entries_2.append(entry)
+    else:
+        entries_1.append(entry)
 
 
 def index(request):
-    return render(request, 'homepage.html', {'blog': entries,
-                                             'random_screenshots': random.sample(_screenshots, 4)})
+    return render(request, 'homepage.html', {
+        'entries_1': entries_1,
+        'entries_2': entries_2,
+        'random_screenshots': random.sample(_screenshots, 4)})
+
+
+def download(request):
+    return render(request, 'download.html')
+
+
+def community(request):
+    return render(request, 'download.html')
--- orange_web/urls.py
+++ orange_web/urls.py
@@ -3,12 +3,12 @@ from django.views.generic import TemplateView
 
 from homepage import views
 
-urlpatterns = patterns(
-    '',
-   url(r'^$', views.index, name='index'),
-   url(r'^screenshots/$', views.screenshots, name='screenshots'),
-   url(r'^license/$', views.license, name='license'),
-   url(r'^contributing/$', views.contribute, name='contribute'),
-   url(r'^download/$', TemplateView.as_view(template_name='download.html'), name='download'),
-   url(r'^community/$', TemplateView.as_view(template_name='community.html'), name='community'),
-   )
+urlpatterns = \
+    patterns('',
+             url(r'^$', views.index, name='index'),
+             url(r'^screenshots/$', views.screenshots, name='screenshots'),
+             url(r'^license/$', views.license, name='license'),
+             url(r'^contributing/$', views.contribute, name='contribute'),
+             url(r'^download/$', views.download, name='download'),
+             url(r'^community/$', views.community, name='community'),
+            )
