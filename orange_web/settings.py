dc67894788b325480e893be526012a68450b9215
Miha Stajdohar <miha.stajdohar@gmail.com>
2014-06-24 09:46:55 +0200

    Changed static root.

--- orange_web/settings.py
+++ orange_web/settings.py
@@ -100,4 +100,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'homepage', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
