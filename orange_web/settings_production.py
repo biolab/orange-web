fc26a9d9cda83c982318ba7c2786869f72b9df8b
Miha Jenko <mjenko@t-2.net>
2014-07-02 14:49:07 +0200

    Fixed issues.

--- orange_web/settings_production.py
+++ orange_web/settings_production.py
@@ -2,7 +2,5 @@ from orange_web.settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-
-DOWNLOAD_SET_PATTERN = os.path.join('/srv/download', 'filenames_%s.set')
-
 ALLOWED_HOSTS = ['orange.biolab.si', 'new.orange.biolab.si']
+DOWNLOAD_SET_PATTERN = os.path.join('/srv/download', 'filenames_%s.set')
