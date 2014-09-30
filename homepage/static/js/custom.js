8e690b3d49d34c2c65960743280d889645e41af7
Miha Jenko <mjenko@t-2.net>
2014-09-30 17:00:25 +0200

    Fixed content (removed forum, development, added stack overflow, facebook to community.html), added class content styling to pages that lacked it, fixed loading of jquery-visage and facebook button.

--- homepage/static/js/custom.js
+++ homepage/static/js/custom.js
@@ -1,4 +1,4 @@
-/* ======= Animations ======= */
+/* ======= Custom js ======= */
 jQuery(document).ready(function($) {
 
     // Allow tooltips only when using non-mobile devices
@@ -7,24 +7,28 @@ jQuery(document).ready(function($) {
         $('.alert').alert();
     }
 
-    /* Facebook */
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    function loadFBGP() {
+        /* Facebook */
+        if (!/127\.0\.0\.1/.test(window.location.hostname) || window.location.pathname.length > 1) return;
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
 
-    /* Google +1 */
-    window.___gcfg = {
-      lang: 'en-US',
-      parsetags: 'onload'
-    };
-    (function() {
-        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-        po.src = 'https://apis.google.com/js/plusone.js?onload=onLoadCallback';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-    })();
+        /* Google +1 */
+        window.___gcfg = {
+          lang: 'en-US',
+          parsetags: 'onload'
+        };
+        (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js?onload=onLoadCallback';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+        })();
+    }
+    loadFBGP();
 });
