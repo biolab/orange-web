f660438f969c3330ca781264a911822b1a540ca8
Miha Jenko <mjenko@t-2.net>
2014-10-03 08:31:39 +0200

    Fixed broken css file, added 'use strict' to js.

--- homepage/static/js/custom.js
+++ homepage/static/js/custom.js
@@ -1,5 +1,6 @@
 /* ======= Custom js ======= */
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+    'use strict';
 
     // Allow tooltips only when using non-mobile devices
     if (jQuery.browser.mobile === false) {
@@ -8,10 +9,14 @@ jQuery(document).ready(function($) {
 
     function loadFBGP() {
         /* Facebook */
-        if (!/127\.0\.0\.1/.test(window.location.hostname) || window.location.pathname.length > 1) return;
+        if (!/127\.0\.0\.1/.test(window.location.hostname) || window.location.pathname.length > 1) {
+            return;
+        }
         (function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
+            if (d.getElementById(id)) {
+                return;
+            }
             js = d.createElement(s);
             js.id = id;
             js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
--- homepage/static/js/main.js
+++ homepage/static/js/main.js
@@ -1,4 +1,5 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+    'use strict';
 
     /* ======= Flexslider ======= */
     $('.flexslider').flexslider({
