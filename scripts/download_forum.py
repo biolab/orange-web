8e8197eba556e4be195719f73043de7fd620902f
astaric <anze.staric@gmail.com>
2014-12-10 16:20:38 +0100

    Refactoring.

--- scripts/download_forum.py
+++ scripts/download_forum.py
@@ -1,56 +1,49 @@
 import os
+import re
 import urllib.request
 from bs4 import BeautifulSoup
 
 
 base_url = 'http://orange.biolab.si/forum/'
 base_dir = os.path.dirname(__file__)
-queue = ['']
-processed = {''}
+
+forum_url = re.compile('^/forum/|^./')
+sid = re.compile('[\?\&]sid=[a-zA-Z0-9]+')
+skip_urls = re.compile(
+    '^search.php|^ucp.php|^posting.php|^mailto:|^/|^memberlist.php')
+
+def escape_question_marks(s):
+    return s.replace('?', '_qm_')
 
 def extract_links(soup):
     for link in soup.find_all('a'):
         href = link.get('href')
-
         if not href:
             continue
 
         if href.startswith('http'):
             yield href
-        if href.startswith('/'):
-            if href.startswith('/forum/'):
-                href = href[7:]
-            else:
-                href = '#'
-        if href.startswith('./'):
-            href = href[2:]
-        try:
-            sid_idx = href.index('sid=')
-            href = href[:sid_idx-1]
-        except ValueError:
-            pass
-        if href == 'index.php':
-            href = ''
-        if href.startswith('search.php'):
+
+        href = forum_url.sub('', href)
+        href = sid.sub('', href)
+
+        if skip_urls.match(href):
             href = '#'
-        if href.startswith('ucp.php'):
-            href = '#'
-        if href.startswith('posting.php'):
-            href = '#'
-        if href.startswith('mailto:'):
-            href = '#'
+
         if href != '#':
             if '#' in href:
                 href = href[:href.index('#')]
             yield href
-        link['href'] = href.replace('?', '_qm_')
+        link['href'] = escape_question_marks(href)
 
-
+queue = ['']
+processed = {''}
 while queue:
     filename = queue.pop()
     url = base_url + filename
     print(url)
-    path = os.path.join(base_dir, filename.replace('?', '_qm_') or 'index.php')
+    path = os.path.join(base_dir,
+                        escape_question_marks(filename) or 'index.php')
 
     urllib.request.urlretrieve(url, path)
 
