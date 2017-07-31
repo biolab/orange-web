6b7cf8fc9d845a5f360c5eb72610975d6f319bae
Ajda Pretnar <ajdapretnar@gmail.com>
2017-07-31 15:33:34 +0200

    Installation Instructions

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^text-analysis/$', TemplateView.as_view(template_name='courses/text-analysis.html')),
+    url(r'^digital-humanities-2017/$', TemplateView.as_view(template_name='courses/digital-humanities-2017.html')),
 ]
