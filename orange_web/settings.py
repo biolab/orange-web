43a0ea7893d0925dc6ae2fa358e38dce373c0b59
astaric <anze.staric@gmail.com>
2017-08-17 09:45:46 +0200

    maint: Remove sqlite db from version control

--- orange_web/settings.py
+++ orange_web/settings.py
@@ -79,7 +79,7 @@ ADMINS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'local.db'),
     }
 }
 # Internationalization
