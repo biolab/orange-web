5956f5a9a32209019e03a1a1bd796ad113480279
astaric <anze.staric@gmail.com>
2017-10-17 10:53:01 +0200

    settings: allow override with ENV variables

--- orange_web/settings.py
+++ orange_web/settings.py
@@ -22,13 +22,15 @@ LICENSE_FILE = os.path.join(BASE_DIR, 'LICENSES')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'input a random string'
-RECAPTCHA_SECRET = 'get the string from Google'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 'input a random string')
+RECAPTCHA_SECRET = os.environ.get(
+    "RECAPTCHA_SECRET", 'get the string from Google')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # FOR TESTING WHEN FALSE: python manage.py runserver --insecure
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") in ["True", "1"]
 
 ALLOWED_HOSTS = []
 
@@ -114,15 +116,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-DOWNLOAD_DIR = os.path.abspath("./download")
-DOWNLOAD_SET_PATTERN = os.path.join(DOWNLOAD_DIR, "filenames_%s.set")
-WIDGET_CATALOG = os.path.abspath("./homepage/static/widgets.json")
-ADDON_WIDGET_CATALOG = os.path.abspath("./homepage/static/")
-FEATURES_CATALOG = os.path.abspath("./homepage/static/features.json")
-TESTIMONIALS_CATALOG = os.path.abspath("./homepage/static/testimonials.json")
+DOWNLOAD_DIR = os.environ.get(
+    "DOWNLOAD_DIR", os.path.abspath("./download"))
+DOWNLOAD_SET_PATTERN = os.environ.get(
+    "DOWNLOAD_SET_PATTERN", os.path.join(DOWNLOAD_DIR, "filenames_%s.set"))
+WIDGET_CATALOG = os.environ.get(
+    "WIDGET_CATALOG", os.path.abspath("./homepage/static/widgets.json"))
+ADDON_WIDGET_CATALOG = os.environ.get(
+    "ADDON_WIDGET_CATALOG", os.path.abspath("./homepage/static/"))
+FEATURES_CATALOG = os.environ.get(
+    "FEATURES_CATALOG", os.path.abspath("./homepage/static/features.json"))
+TESTIMONIALS_CATALOG = os.environ.get(
+    "TESTIMONIALS_CATALOG", os.path.abspath("./homepage/static/testimonials.json"))
 
 # Error report settings
-ERROR_REPORT_DIR = os.path.abspath("./error_report/")
+ERROR_REPORT_DIR = os.environ.get(
+    "ERROR_REPORT_DIR", os.path.abspath("./error_report/"))
 # Biolab's testing Sentry project
 ERROR_REPORT_SENTRY_DSN_ORANGE = 'https://261797e8fa4544ffb931bc495157d2e3:44e30b93f9f1463a975725f82ca18039@sentry.io/128442'
 
